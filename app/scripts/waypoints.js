62edc9c3fc087cb96c2b2f9b342346be90d7784f
R.J. LaCount <rjlacount@gmail.com>
2016-10-12 11:09:48 -0700

    Detect currently active area with waypoints

--- app/scripts/waypoints.js
+++ app/scripts/waypoints.js
@@ -1,12 +1,34 @@
 export default function() {
 
-  var navbar = document.getElementById('navbar');
+  var navbar = document.getElementById('navbar'),
+      sections = ['hero-area', 'about-me', 'my-work', 'contact'];
 
-  var waypoint = new Waypoint({
-    element: document.getElementById('about-me'),
-    handler: function(direction) {
-      direction === 'down' ? navbar.classList.add('active') : navbar.classList.remove('active');
-    },
-    offset: 96
+  function showNavBar(direction) {
+    direction === 'down' ? navbar.classList.add('active') : navbar.classList.remove('active');
+  }
+
+  function setActiveNavItem(section) {
+    sections.forEach(function(value) {
+      navbar.classList.remove(value);
+    });
+    navbar.classList.add(section);
+  }
+
+  function newSectionInView(section, index, direction) {
+    if (section === 'about-me') { showNavBar(direction); }
+    direction === 'down' ? setActiveNavItem(sections[index]) : setActiveNavItem(sections[index - 1]);
+  }
+
+  function setWaypoint(elementId, elementIndex, offsetValue) {
+    new Waypoint({
+      element: document.getElementById(elementId),
+      handler: function(direction) {
+        newSectionInView(elementId, elementIndex, direction);
+      },
+      offset: offsetValue
+    });
+  }
+  sections.forEach(function(value, index) {
+    setWaypoint(value, index, 96);
   });
 }
