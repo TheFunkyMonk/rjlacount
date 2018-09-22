96375d5d84bdc3ea9c54b9cc2bedc91973e09177
R.J. LaCount <rjlacount@gmail.com>
2018-09-21 18:43:17 -0700

    Fix waypoints bug with fixed hero area

--- app/scripts/waypoints.js
+++ app/scripts/waypoints.js
@@ -1,34 +1,39 @@
-export default function() {
-
+export default function () {
   var navbar = document.getElementById('navbar'),
-      sections = ['hero-area', 'about-me', 'my-work', 'contact'];
+    sections = ['about-me', 'my-work', 'contact'];
 
   function showNavBar(direction) {
-    direction === 'down' ? navbar.classList.add('active') : navbar.classList.remove('active');
+    direction === 'down'
+      ? navbar.classList.add('active')
+      : navbar.classList.remove('active');
   }
 
   function setActiveNavItem(section) {
-    sections.forEach(function(value) {
+    sections.forEach(function (value) {
       navbar.classList.remove(value);
     });
     navbar.classList.add(section);
   }
 
   function newSectionInView(section, index, direction) {
-    if (section === 'about-me') { showNavBar(direction); }
-    direction === 'down' ? setActiveNavItem(sections[index]) : setActiveNavItem(sections[index - 1]);
+    if (section === 'about-me') {
+      showNavBar(direction);
+    }
+    direction === 'down'
+      ? setActiveNavItem(sections[index])
+      : setActiveNavItem(sections[index - 1] || sections[index]);
   }
 
   function setWaypoint(elementId, elementIndex, offsetValue) {
     new Waypoint({
       element: document.getElementById(elementId),
-      handler: function(direction) {
+      handler: function (direction) {
         newSectionInView(elementId, elementIndex, direction);
       },
       offset: offsetValue
     });
   }
-  sections.forEach(function(value, index) {
+  sections.forEach(function (value, index) {
     setWaypoint(value, index, 96);
   });
 }
